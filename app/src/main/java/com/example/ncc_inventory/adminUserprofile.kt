50193f2a530c0108e32da75f296ddd3d1b96b7f2
Rahul Dasila <dasilarahul73@gmail.com>
2024-06-21 10:43:44 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/ncc_inventory/adminUserprofile.kt
@@ -0,0 +1,11 @@
+package com.example.ncc_inventory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class adminUserprofile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_userprofile)
+    }
+}
