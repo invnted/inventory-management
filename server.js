58a3d945f71fff16712e99c890299c703d89d259
Ayushkothari47 <ayushkotharii47@gmail.com>
2024-06-22 17:30:46 +0530

    commit

--- server.js
+++ server.js
@@ -25,6 +25,6 @@ app.use('/managers',userRoutes);
 
 
 const PORT = 5000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server  started on port ${PORT}`));
 
 
