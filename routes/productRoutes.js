a055e03db187de87faa0d82fd9fc83174e14885d
Ayushkothari47 <ayushkotharii47@gmail.com>
2024-07-01 11:56:30 +0530

    Additional Routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -10,6 +10,9 @@ router.post('/add', productController.addProduct);
 router.get('/get-products-csv',productController.getProductCSV)
 router.post('/upload-product-csv', upload.single('csvFile'), productController.uploadCSV);
 router.post('/productType-list',productController.getAllProductsType);
+router.post('/productModel-list',productController.getAllProductsModel);
+router.post('/productBrand-list',productController.getAllProductsBrand);
+
 router.post('/getProductStore',productController.getProductStore);
 router.post('/makeDemand',productController.makeDemand);
 router.post('/getUserDemand',productController.getUserDemands);
