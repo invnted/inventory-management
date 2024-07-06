7351c23f077a484667f373b7024a38dfca5a1c8b
Ayushkothari47 <ayushkotharii47@gmail.com>
2024-07-06 12:28:40 +0530

    modification in manager login

--- controllers/userController.js
+++ controllers/userController.js
@@ -171,7 +171,7 @@ exports.loginManager = async (req, res) => {
     jwt.sign(payload, "7583d88d1f4614edf7e3d4b52e496a0fb02fbc1885bb64b06d62257549ee0a1929fa5a52e14e979587536fee27e7f20980719862c1c1664b3461e3eaa9c9f9c1", { expiresIn: 3600 }, (err, token) => {
       if (err) throw err;
       const { managerId, managerName, password, designation, section, appointment, allProductReport, demandReceived, issueProduct } = manager;
-      res.json({ token, manager: { managerId, managerName, password, designation, section, appointment, allProductReport, demandReceived, issueProduct } });
+      res.json({ token,success:true, manager: { managerId, managerName, password, designation, section, appointment, allProductReport, demandReceived, issueProduct } });
     });
   } catch (err) {
     console.error(err.message);
